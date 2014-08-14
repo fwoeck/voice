a22d70e11ba9e4a5fcc90bc0dfb38ab21ecbbade
Frank Wöckener <fwoeck@gmail.com>
2014-08-14 06:45:24 +0200

    update seeds for chef-solo

---
+++ seeds/solo.rb
@@ -0,0 +1,7 @@
+log_level         :warn
+verbose_logging   false
+ssl_verify_mode   :verify_peer
+json_attribs      '/etc/chef/node.json'
+role_path         '/opt/voice-chef/roles'
+cookbook_path     '/opt/voice-chef/cookbooks'
+environment_path  '/opt/voice-chef/environments'
