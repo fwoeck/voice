00f94f02dc9df4519d17770ad3932e938fd7125f
Frank Woeckener <fwoeck@gmail.com>
2014-10-12 14:27:45 +0200

    update locations

--- cookbooks/voice-specs/recipes/default.rb
+++ cookbooks/voice-specs/recipes/default.rb
@@ -7,3 +7,23 @@ git node[:voice_specs][:basedir] do
 
   not_if "test -e #{node[:voice_specs][:basedir]}"
 end
+
+template "#{node[:voice_specs][:basedir]}/config/app.yml.example" do
+  source 'app.yml.erb'
+  owner   node[:wim][:user]
+  group   node[:wim][:group]
+end
+
+file "#{node[:voice_specs][:basedir]}/.ruby-version" do
+  content "ruby-#{node[:mri][:version]}"
+  owner   node[:wim][:user]
+  group   node[:wim][:group]
+  mode    00755
+end
+
+template "#{node[:voice_specs][:basedir]}/.bundle/config" do
+  source   'bundle.config.erb'
+  cookbook 'etc'
+  owner     node[:wim][:user]
+  group     node[:wim][:group]
+end
